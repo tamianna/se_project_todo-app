8c58420e3af9315db5b85a15ba511d3890c17349
Tamianna <tamianna.h@gmail.com>
2025-04-06 14:42:25 -0500

    feat: add components and utils folder

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,4 @@
-const initialTodos = [
-  {
-    id: "7cec7373-681b-49d9-b065-021d61a69d03",
-    name: "Read the sprint's theory",
-    completed: true,
-    date: new Date(),
-  },
-  {
-    id: "a7bfd5ef-37cc-4fa6-89f2-cac098a8aeba",
-    name: "Read project instructions",
-    completed: false,
-    date: new Date(),
-  },
-  {
-    id: "aa486839-63ab-437f-b8a2-29ab217dff4f",
-    name: "Complete project",
-    completed: false,
-    date: new Date(),
-  },
-];
-
-const validationConfig = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  errorClass: "popup__error_visible",
-  inputErrorClass: "popup__input_type_error",
-  inactiveButtonClass: "button_disabled",
-};
+import { iniTodos, valConfig } from "../utils/constants.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -103,7 +75,7 @@ addTodoForm.addEventListener("submit", (evt) => {
   closeModal(addTodoPopup);
 });
 
-initialTodos.forEach((item) => {
+iniTodos.forEach((item) => {
   const todo = generateTodo(item);
   todosList.append(todo);
 });
